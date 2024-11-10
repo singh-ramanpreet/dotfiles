ab8695be81eb6bbbb012dec76d9d1ee39e1529be
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2024-11-09 17:49:59 -0800

    Enhance SSH agent setup in profile.sh to clean up old sockets and add keys if present

--- bash/profile.sh
+++ bash/profile.sh
@@ -10,10 +10,21 @@ if [[ -f ~/.bashrc ]]; then
 fi
 
 # ssh agent
+# clean up old sockets 
+# start ssh-agent if not running
 if [[ "$(ps -u $USER | grep ssh-agent | wc -l)" -lt "1" || ! -S $HOME/.ssh/ssh_auth_sock ]]; then
   find /tmp -maxdepth 1 -type d -name "ssh-*" -exec rm -rv {} \;
   eval $(ssh-agent -s)
   ln -sf "$SSH_AUTH_SOCK" $HOME/.ssh/ssh_auth_sock
 fi
+
+# set the socket
 export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
-ssh-add -l >/dev/null || ssh-add $(ls $HOME/.ssh/*.pub | sed 's/.pub//')
+
+# if keys present, add them
+if [[ -f $HOME/.ssh/id_rsa.pub ]]; then
+  ssh-add -l >/dev/null || ssh-add $HOME/.ssh/id_rsa
+fi
+if [[ -f $HOME/.ssh/id_ed25519.pub ]]; then
+  ssh-add -l >/dev/null || ssh-add $HOME/.ssh/id_ed25519
+fi
