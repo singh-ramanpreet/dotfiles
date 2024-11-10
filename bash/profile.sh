da2070898d7ef86c44dd0e3e67d5087eea85533e
Ramanpreet Singh <singh.ramanpreet09@gmail.com>
2024-11-09 18:30:20 -0800

    Refactor SSH agent setup in profile.sh to improve socket handling and ensure keys are added if present

--- bash/profile.sh
+++ bash/profile.sh
@@ -9,17 +9,24 @@ if [[ -f ~/.bashrc ]]; then
   . ~/.bashrc
 fi
 
-# ssh agent
-# clean up old sockets 
-# start ssh-agent if not running
-if [[ "$(ps -u $USER | grep ssh-agent | wc -l)" -lt "1" || ! -S $HOME/.ssh/ssh_auth_sock ]]; then
-  find /tmp -maxdepth 1 -type d -name "ssh-*" -exec rm -rv {} \;
-  eval $(ssh-agent -s)
-  ln -sf "$SSH_AUTH_SOCK" $HOME/.ssh/ssh_auth_sock
-fi
+# create the socket file on the host with keys
+# example:
+#   nc -U $HOME/.ssh/ssh_auth_sock -l -k &
+#   kill -9 $(lsof -t -U $HOME/.ssh/ssh_auth_sock)
 
-# set the socket
-export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
+# check if socket file exists
+# if it does, find the running ssh-agent and
+# set the socket to the one in the file
+# if it doesn't, start a new ssh-agent and
+# clean up old sockets
+if [[ -S $HOME/.ssh/ssh_auth_sock ]]; then
+  if [[ "$(ps -u $USER | grep ssh-agent | wc -l)" -lt "1" ]] then
+    find /tmp -maxdepth 1 -type d -name "ssh-*" -exec rm -rv {} \;
+    eval $(ssh-agent -s)
+    ln -sf "$SSH_AUTH_SOCK" $HOME/.ssh/ssh_auth_sock
+  fi
+  export SSH_AUTH_SOCK=$HOME/.ssh/ssh_auth_sock
+fi
 
 # if keys present, add them
 if [[ -f $HOME/.ssh/id_rsa.pub ]]; then
